5e7f70681e8f115189de5422e8862c9bbfb0d057
vietdss <nguyenhoangviet251103@gmail.com>
2026-04-01 16:04:08 +0700

    chore: update default admin email address in AdminSeeder

--- database/seeders/AdminSeeder.php
+++ database/seeders/AdminSeeder.php
@@ -12,7 +12,7 @@ class AdminSeeder extends Seeder
     {
         Admin::create([
             'name' => 'Super Admin',
-            'email' => 'admin@admin.com',
+            'email' => 'admin@example.com',
             'password' => Hash::make('password'),
             'role' => 'super_admin',
             'status' => 'active',
